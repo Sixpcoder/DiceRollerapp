8d7a5f5bb330c966523c4774ffabdb8255ca116b
sixpcoder <alirounak999@gmail.com>
2023-04-28 10:18:36 +0530

    first commit

---
+++ app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dice_roller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
